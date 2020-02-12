722e49bb5aaf312502371e10d2f5c6944b13b96d
MAKCOH <dramdmg6@gmail.com>
2020-02-12 23:16:31 +0700

    Vector: Внес все исправления и удалил старую версию

--- Vector/src/name/max_ferrara/vector/Vector.java
+++ Vector/src/name/max_ferrara/vector/Vector.java
@@ -3,74 +3,49 @@ package name.max_ferrara.vector;
 import java.util.Arrays;
 
 public class Vector {
-    private int vectorSize;
-    private double[] vectorCoordinates;
+    private double[] coordinates;
 
-    public Vector(int vectorSize) {
-        if (vectorSize <= 0) {
-            throw new IllegalArgumentException("vectorSize can not <= zero");
+    public Vector(int length) {
+        if (length < 1) {
+            throw new IllegalArgumentException("vectorLength can not < 1");
         }
 
-        double[] tmp = new double[vectorSize];
-
-        Arrays.fill(tmp, 0);
-
-        this.vectorSize = vectorSize;
-        this.vectorCoordinates = tmp;
+        coordinates = new double[length];
     }
 
     public Vector(double[] vectorCoordinates) {
-        this.vectorCoordinates = vectorCoordinates;
+        if (vectorCoordinates.length < 1) {
+            throw new IllegalArgumentException("vectorLength can not < 1");
+        }
+
+        coordinates = Arrays.copyOf(vectorCoordinates, vectorCoordinates.length);
     }
 
-    public Vector(double[] vectorCoordinates, int vectorSize) {
-        if (vectorSize <= 0) {
-            throw new IllegalArgumentException("vectorSize can not <= zero");
+    public Vector(int vectorLength, double[] vectorCoordinates) {
+        if (vectorCoordinates.length < 1) {
+            throw new IllegalArgumentException("vectorLength can not < 1");
         }
 
-        double[] tmp = new double[vectorSize];
-        int lengthValue = tmp.length - vectorCoordinates.length;
+        double[] tmp = new double[vectorLength];
 
-        for (int i = 0; i < tmp.length - lengthValue; i++) {
-            if (tmp[i] > vectorCoordinates.length) {
-                tmp[i] = 0;
-            }
-
-            tmp[i] = vectorCoordinates[i];
-        }
-
-        this.vectorSize = vectorSize;
-        this.vectorCoordinates = tmp;
+        coordinates = Arrays.copyOf(vectorCoordinates, tmp.length);
     }
 
     public Vector(Vector vector) {
-        vectorSize = vector.vectorSize;
-        vectorCoordinates = vector.vectorCoordinates;
-    }
-
-    public double[] getVectorCoordinates() {
-        return vectorCoordinates;
-    }
-
-    public void setVectorCoordinates(double[] vectorCoordinates) {
-        this.vectorCoordinates = vectorCoordinates;
-    }
-
-    public int getVectorSize() {
-        return vectorSize;
-    }
-
-    public void setVectorSize(int vectorSize) {
-        if (vectorSize <= 0) {
-            throw new IllegalArgumentException("vectorSize can not <= zero");
-        }
-
-        this.vectorSize = vectorSize;
+        coordinates = Arrays.copyOf(vector.coordinates, vector.coordinates.length);
     }
 
     @Override
     public String toString() {
-        return Arrays.toString(vectorCoordinates);
+        StringBuilder formatCoordinates = new StringBuilder();
+
+        for (double coordinate : coordinates) {
+            formatCoordinates.append(coordinate).append(", ");
+        }
+
+        formatCoordinates.setLength(formatCoordinates.length() - 2);
+
+        return "{ " + formatCoordinates.toString() + " }";
     }
 
     @Override
@@ -85,7 +60,7 @@ public class Vector {
 
         Vector vector = (Vector) object;
 
-        return vectorSize == vector.vectorSize && Arrays.equals(vectorCoordinates, vector.vectorCoordinates);
+        return Arrays.equals(coordinates, vector.coordinates);
     }
 
     @Override
@@ -93,116 +68,120 @@ public class Vector {
         final int prime = 21;
         int hash = 1;
 
-        hash *= prime + vectorSize;
-        hash *= prime + Arrays.hashCode(vectorCoordinates);
+        hash = prime * hash + Arrays.hashCode(coordinates);
         return hash;
     }
 
     public double getElementIndex(int index) {
-        return vectorCoordinates[index];
+        return coordinates[index];
     }
 
-    public int getLength() {
-        return vectorCoordinates.length;
+    public void scale(double number) {
+        for (int i = 0; i < coordinates.length; ++i) {
+            coordinates[i] *= number;
+        }
     }
 
     public void reverse() {
-        for (int i = 0; i < vectorCoordinates.length; ++i) {
-            vectorCoordinates[i] *= -1;
+        scale(-1);
+    }
+
+    public double getLength() {
+        double vectorLength = 0;
+
+        for (double coordinate : coordinates) {
+            vectorLength = vectorLength + Math.pow(coordinate, 2);
+        }
+
+        return Math.sqrt(vectorLength);
+    }
+
+    public void subtraction(Vector vector) {
+        if (coordinates.length > vector.coordinates.length) {
+            double[] tmp = new double[Math.max(coordinates.length, vector.coordinates.length)];
+            vector.coordinates = Arrays.copyOf(vector.coordinates, tmp.length);
+
+            for (int i = 0; i < coordinates.length; ++i) {
+                coordinates[i] = coordinates[i] - vector.coordinates[i];
+            }
+        } else if (coordinates.length < vector.coordinates.length) {
+            double[] tmp = new double[Math.max(coordinates.length, vector.coordinates.length)];
+            coordinates = Arrays.copyOf(coordinates, tmp.length);
+
+            for (int i = 0; i < coordinates.length; ++i) {
+                coordinates[i] = coordinates[i] - vector.coordinates[i];
+            }
+        } else {
+            for (int i = 0; i < coordinates.length; ++i) {
+                coordinates[i] = coordinates[i] - vector.coordinates[i];
+            }
         }
     }
 
-    public void scaling(double number) {
-        for (int i = 0; i < vectorCoordinates.length; ++i) {
-            vectorCoordinates[i] *= number;
+    public void summarize(Vector vector) {
+        if (coordinates.length > vector.coordinates.length) {
+            double[] tmp = new double[Math.max(coordinates.length, vector.coordinates.length)];
+            vector.coordinates = Arrays.copyOf(vector.coordinates, tmp.length);
+
+            for (int i = 0; i < coordinates.length; ++i) {
+                coordinates[i] = coordinates[i] + vector.coordinates[i];
+            }
+        } else if (coordinates.length < vector.coordinates.length) {
+            double[] tmp = new double[Math.max(coordinates.length, vector.coordinates.length)];
+            coordinates = Arrays.copyOf(coordinates, tmp.length);
+
+            for (int i = 0; i < coordinates.length; ++i) {
+                coordinates[i] = coordinates[i] + vector.coordinates[i];
+            }
+        } else {
+            for (int i = 0; i < coordinates.length; ++i) {
+                coordinates[i] = coordinates[i] + vector.coordinates[i];
+            }
         }
+    }
+
+    public static double getScalarComposition(Vector vector1, Vector vector2) {
+        if (vector1.coordinates.length > vector2.coordinates.length) {
+            double[] tmp = new double[Math.max(vector1.coordinates.length, vector2.coordinates.length)];
+            vector2.coordinates = Arrays.copyOf(vector2.coordinates, tmp.length);
+
+            for (int i = 0; i < tmp.length; ++i) {
+                tmp[i] = vector1.coordinates[i] * vector2.coordinates[i];
+            }
+
+            return Arrays.stream(tmp).sum();
+        }
+
+        if (vector1.coordinates.length < vector2.coordinates.length) {
+            double[] tmp = new double[Math.max(vector1.coordinates.length, vector2.coordinates.length)];
+            vector1.coordinates = Arrays.copyOf(vector1.coordinates, tmp.length);
+
+            for (int i = 0; i < tmp.length; ++i) {
+                tmp[i] = vector1.coordinates[i] * vector2.coordinates[i];
+            }
+
+            return Arrays.stream(tmp).sum();
+        }
+
+        double[] tmp = new double[vector1.coordinates.length];
+
+        for (int i = 0; i < tmp.length; ++i) {
+            tmp[i] = vector1.coordinates[i] * vector2.coordinates[i];
+
+        }
+
+        return Arrays.stream(tmp).sum();
     }
 
     public static Vector getSum(Vector vector1, Vector vector2) {
-        double[] vectorsSum = new double[Math.max(vector1.vectorCoordinates.length, vector2.vectorCoordinates.length)];
+        vector1.summarize(vector2);
 
-        for (int i = 0; i < vectorsSum.length; ++i) {
-            vectorsSum[i] = 0;
-
-            if (i >= Math.min(vector1.vectorCoordinates.length, vector2.vectorCoordinates.length)) {
-                vectorsSum[i] = getMaxLengthVector(vector1.vectorCoordinates, vector2.vectorCoordinates)[i];
-            } else {
-                vectorsSum[i] = vector1.vectorCoordinates[i] + vector2.vectorCoordinates[i];
-            }
-        }
-
-        return new Vector(vectorsSum);
+        return new Vector(vector1);
     }
 
     public static Vector getDifference(Vector vector1, Vector vector2) {
-        double[] vectorsDifference = new double[Math.max(vector1.vectorCoordinates.length, vector2.vectorCoordinates.length)];
+        vector1.subtraction(vector2);
 
-        for (int i = 0; i < vectorsDifference.length; ++i) {
-            vectorsDifference[i] = 0;
-
-            if (i >= Math.min(vector1.vectorCoordinates.length, vector2.vectorCoordinates.length)) {
-                vectorsDifference[i] = -getMaxLengthVector(vector1.vectorCoordinates, vector2.vectorCoordinates)[i];
-            } else {
-                vectorsDifference[i] = vector1.vectorCoordinates[i] - vector2.vectorCoordinates[i];
-            }
-        }
-
-        return new Vector(vectorsDifference);
-    }
-
-    public static Vector getScalarComposition(Vector vector1, Vector vector2) {
-        double[] vectorsScalarComposition = new double[Math.max(vector1.vectorCoordinates.length, vector2.vectorCoordinates.length)];
-
-        for (int i = 0; i < vectorsScalarComposition.length; ++i) {
-            vectorsScalarComposition[i] = 0;
-
-            if (i >= Math.min(vector1.vectorCoordinates.length, vector2.vectorCoordinates.length)) {
-                vectorsScalarComposition[i] = 0;
-            } else {
-                vectorsScalarComposition[i] = vector1.vectorCoordinates[i] * vector2.vectorCoordinates[i];
-            }
-        }
-
-        return new Vector(vectorsScalarComposition);
-    }
-
-    public double[] getSum(Vector vector) {
-        double[] tmp = new double[Math.max(vectorCoordinates.length, vector.vectorCoordinates.length)];
-
-        for (int i = 0; i < tmp.length; ++i) {
-            tmp[i] = 0;
-
-            if (i >= Math.min(vectorCoordinates.length, vector.vectorCoordinates.length)) {
-                tmp[i] = getMaxLengthVector(vectorCoordinates, vector.vectorCoordinates)[i];
-            } else {
-                tmp[i] = vectorCoordinates[i] + vector.vectorCoordinates[i];
-            }
-        }
-
-        vectorCoordinates = tmp;
-
-        return vectorCoordinates;
-    }
-
-    public double[] getDifference(Vector vector) {
-        double[] tmp = new double[Math.max(vectorCoordinates.length, vector.vectorCoordinates.length)];
-
-        for (int i = 0; i < tmp.length; ++i) {
-            tmp[i] = 0;
-
-            if (i >= Math.min(vectorCoordinates.length, vector.vectorCoordinates.length)) {
-                tmp[i] = -getMaxLengthVector(vectorCoordinates, vector.vectorCoordinates)[i];
-            } else {
-                tmp[i] = vectorCoordinates[i] - vector.vectorCoordinates[i];
-            }
-        }
-
-        vectorCoordinates = tmp;
-
-        return vectorCoordinates;
-    }
-
-    private static double[] getMaxLengthVector(double[] vectorDirection1, double[] vectorDirection2) {
-        return (vectorDirection1.length > vectorDirection2.length) ? vectorDirection1 : vectorDirection2;
+        return new Vector(vector1);
     }
 }
--- Vector/src/name/max_ferrara/vector_main/VectorMain.java
+++ Vector/src/name/max_ferrara/vector_main/VectorMain.java
@@ -2,15 +2,13 @@ package name.max_ferrara.vector_main;
 
 import name.max_ferrara.vector.Vector;
 
-import java.util.Arrays;
-
 public class VectorMain {
     public static void main(String[] args) {
         try {
             Vector vector1 = new Vector(6);
             Vector vector2 = new Vector(new double[]{10, 4, 13, 9.65, 3, 14});
             Vector vector3 = new Vector(new double[]{-4, 25, 2.5, 0, 135, 27, 5, 9, 15, 56});
-            Vector vector4 = new Vector(new double[]{10, 4, 13, 9.65, 3, 14}, 10);
+            Vector vector4 = new Vector(10, new double[]{10, 4, 13, 9.65, 3, 14});
             Vector cloneVector = new Vector(vector2);
 
             System.out.println(vector1);
@@ -28,7 +26,7 @@ public class VectorMain {
             System.out.println(vector3.getLength());
             vector2.reverse();
             System.out.println(vector2);
-            vector2.scaling(1.25);
+            vector2.scale(1.25);
             System.out.println(vector2);
             System.out.println();
 
@@ -37,8 +35,8 @@ public class VectorMain {
             System.out.println(Vector.getScalarComposition(vector2, vector3));
             System.out.println();
 
-            System.out.println(Arrays.toString(vector1.getDifference(vector3)));
-            System.out.println(Arrays.toString(vector2.getSum(vector3)));
+            vector1.subtraction(vector3);
+            vector2.summarize(vector3);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }

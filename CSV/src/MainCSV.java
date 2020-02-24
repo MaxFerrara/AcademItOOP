adf844bb83c8d5bd98c7c8b5e0201ca74f79a530
MAKCOH <dramdmg6@gmail.com>
2020-02-24 19:56:11 +0700

    Начал делать CSV

---
+++ CSV/src/MainCSV.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class MainCSV {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        try (Scanner scanner = new Scanner(new FileInputStream("C:\\Users\\MAKCOH\\IdeaProjects\\AcademItOOP\\CSV\\CSV_file.txt"));
+             PrintWriter writer = new PrintWriter("C:\\Users\\MAKCOH\\IdeaProjects\\AcademItOOP\\CSV\\HTML_file.html", "UTF-8")) {
+
+            while (scanner.hasNextLine()) {
+                String lowerCaseString = scanner.nextLine();
+                writer.println(lowerCaseString);
+            }
+        }
+    }
+}

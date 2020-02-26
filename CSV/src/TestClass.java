333314453aa41e2c831c63e5cd10795b4c7a266a
MAKCOH <dramdmg6@gmail.com>
2020-02-26 23:35:15 +0700

    CSV: появились начальные наработки по алгоритму

--- CSV/src/TestClass.java
+++ CSV/src/TestClass.java
@@ -1,18 +1,25 @@
 public class TestClass {
     public static void main(String[] args) {
-        String reference = "Maksim,Maksim,Maksim\nSasha,Sasha,Sasha";
+        String reference = "Maksim,Maksim,Maksim Sasha,Sasha,Sasha";
         System.out.println(reference);
 
-        StringBuilder sb = new StringBuilder("<table>");
+        StringBuilder sb = new StringBuilder("<table>\n<tr>\n<th> ");
 
         char[] referenceToArray = reference.toCharArray();
 
         for (int i = 0; i < referenceToArray.length; ++i) {
-            //if(referenceToArray[i] == ",") {
-            //    sb.
-            //}
+            if (referenceToArray[i] == ',') {
+                //sb.deleteCharAt(i + 1);
+                sb.append("</th> \n <th>");
+            }
+
+            if (referenceToArray[i] == ' ') {
+                sb.append("</th>\n</tr>\n<tr>\n<th>");
+            }
             sb.append(referenceToArray[i]);
         }
+
+        sb.append("</th>\n</tr>\n</table>");
         System.out.println(sb);
     }
 }

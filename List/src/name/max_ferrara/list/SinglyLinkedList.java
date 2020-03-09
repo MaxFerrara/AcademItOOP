602e202b756ecf18c2bbb8aee974d0df3cdf5f8d
MAKCOH <dramdmg6@gmail.com>
2020-03-10 01:27:28 +0700

    List: создал модуль и начал разбираться

---
+++ List/src/name/max_ferrara/list/SinglyLinkedList.java
@@ -0,0 +1,12 @@
+package name.max_ferrara.list;
+
+import name.max_ferrara.list_item.ListItem;
+
+public class SinglyLinkedList<T> {
+    private ListItem<T> head;
+    private int size;
+
+    public SinglyLinkedList() {
+    }
+
+}
